24e5c4851da575f150239193b84bd82bb929fe81
Amynem <amy.lauren98@hotmail.com>
2024-11-30 10:37:06 +0100

    ite 2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,12 @@
+import { PageLayout } from "../components/layouts";
+
 function Home() {
     return (
+    <PageLayout>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quos totam quis commodi optio sint temporibus odit sed inventore nisi dolorem maxime quaerat labore aliquam, consectetur aspernatur repellendus tempore ullam.</p>
+    </PageLayout>
     );
+    
 }
 
 export default Home;
